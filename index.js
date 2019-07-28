122f6756ee54462e92ddfdf801d463a22b68bf44
Rashad Saleh <rashad.saleh@gmail.com>
2019-07-28 19:17:54 +0300

    Proper unexclude function

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-module.exports = function exclude (functions) {
-    let queue = [];
-    let pass = Symbol();
+module.exports = {
+    unexclude (f) {
+        if (f.constructor === Array) {
+            return f.map(unexclude);
+        }
+        return f._$_mutually_exclusive_functions_$_target;
+    },
+    exclude (functions) {
+        let queue = [];
 
-    return functions.map(f => {
-        return new Proxy(f, {
-            apply: (target, that, args) => {
-
-                
-                that = {...that, pass};
-                
-                if (args.includes(pass)) return target.apply(that, args);
-
-                let x = new Promise(async (resolve, reject) => {
-                    await queue[queue.length-1];
-                    queue.shift();
-                    return resolve(target.apply(that, args));
-                });
-                queue.push(x);
-                return x;
-            }
+        return functions.map(f => {
+            return new Proxy(f, {
+                get(target, prop) {
+                    if (prop === "_$_mutually_exclusive_functions_$_target") {
+                        return target;
+                    } else {
+                        return target[prop];
+                    }
+                },
+                apply: (target, that, args) => {
+                    let x = new Promise(async (resolve, reject) => {
+                        await queue[queue.length-1];
+                        queue.shift();
+                        return resolve(target.apply(that, args));
+                    });
+                    queue.push(x);
+                    return x;
+                }
+            })
         })
-    })
+    }
 }
